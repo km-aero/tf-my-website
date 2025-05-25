b2d8970d2b6c35a35528e2e6d338b9e947e4fc1d
Kevin Monteiro <kevin.monteiro96@gmail.com>
2025-05-25 23:36:29 +0100

    tf: rename secondary domain to www main

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "vercel_project_domain" "main" {
   redirect_status_code = 307
 }
 
-resource "vercel_project_domain" "secondary" {
+resource "vercel_project_domain" "www_main" {
   project_id = vercel_project.my_website.id
   domain     = "www.kbmm.cloud"
 }
