118cf0d0b969abec9f880a40bb3b89f065f891d1
Kevin Monteiro <kevin.monteiro96@gmail.com>
2025-05-25 22:29:41 +0100

    cd: connect github to vercel project

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
 resource "vercel_project" "my_website" {
   name      = var.project_name
   framework = "nextjs"
+
+  git_repository = {
+    type = "github"
+    repo = "km-aero/my-website"
+  }
 }
